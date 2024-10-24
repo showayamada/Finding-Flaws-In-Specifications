d033e58f94a66d85383629a22c2bbc1b81847c8c
showayamada <yamasho082433@gmail.com>
2024-10-24 18:12:03 +0900

    コード整形

--- src/main.cpp
+++ src/main.cpp
@@ -104,69 +104,6 @@ twa_graph_ptr synchronous_product(vector<twa_graph_ptr> automatons, twa_graph_pt
         }
         producted = p(producted, automatons[i], shared, name);
     }
-
-    // if (left->num_states() == 0) {
-    //     return right;
-    // }
-    // if (right->num_states() == 0) {
-    //     return left;
-    // }
-    // twa_graph_ptr producted = make_twa_graph(shared->get_dict());
-    // producted->copy_ap_of(shared);
-    // auto namer = producted->create_namer<string>();
-
-    // // 同期積の状態を作成
-    // for (size_t left_state = 0; left_state < left->num_states(); left_state++) {
-    //     for (size_t right_state = 0; right_state < right->num_states(); right_state++) {
-    //         unsigned new_state = producted->new_state();
-    //         string name = to_string(left_state) + to_string(right_state);
-    //         cout << "name: " << name << endl;
-    //         cout << namer->alias_state(new_state, name) << endl;
-    //     }
-    // }
-    // auto namer_producted = producted->create_namer<string>();
-    // print_hoa(cout, producted);
-
-    // namer_producted->get_name(45);
-
-    // for (auto left_edge: left->edges()) {
-    //     for (auto right_edge: right->edges()) {
-    //         if (left_edge.src == right_edge.src && left_edge.dst == right_edge.dst) {
-    //             bdd cond = bdd_and(left_edge.cond, right_edge.cond);
-    //             producted->new_edge(left_edge.src, left_edge.dst, cond);
-    //         }
-    //     }
-    // }
-
-    // unsigned pre_l;
-    // for (auto left_edge: left->edges()) {
-    //     cout << "LEFT:: from: " << left_edge.src << " to: " << left_edge.dst << " label: " << left_edge.cond << endl;
-    //     unsigned pre_r;
-    //     for (auto right_edge: right->edges()) {
-    //         cout << "RIGHT:: from: " << right_edge.src << " to: " << right_edge.dst << " label: " << right_edge.cond << endl;
-    //         if (pre_l != left_edge.src && pre_r != right_edge.src) {
-    //             unsigned state = producted->new_state();
-    //             cout << left->create_namer<string>()->get_name(left_edge.src) << endl;
-    //             string name = to_string(left_edge.src) + to_string(right_edge.src);
-    //             cout << "name: " << name << endl;
-    //             namer->alias_state(state, name);
-    //         }                
-    //         pre_r = right_edge.src;
-    //     }
-    //     pre_l = left_edge.src;
-    // }
-
-    // for (auto left_edge: left->edges()) {
-    //     cout << "LEFT:: from: " << left_edge.src << " to: " << left_edge.dst << " label: " << left_edge.cond << endl;
-    //     for (auto right_edge: right->edges()) {
-    //         cout << "RIGHT:: from: " << right_edge.src << " to: " << right_edge.dst << " label: " << right_edge.cond << endl;
-    //         if (left_edge.src == right_edge.src && left_edge.dst == right_edge.dst) {
-    //             bdd cond = bdd_and(left_edge.cond, right_edge.cond);
-    //             producted->new_edge(left_edge.src, right_edge.dst, cond);
-    //         }
-    //     }
-    // }
-
     return producted;
 }
 
@@ -189,7 +126,6 @@ int main() {
     translator trans;
     auto shared = trans.run(f);
     auto dict = shared->get_dict();
-    dict->dump(cout);
 
     vector<string> responseEvents = {"y"};
     vector<twa_graph_ptr> automaton_list = {};
@@ -207,50 +143,29 @@ int main() {
         print_hoa(ofs, automaton);
         ofs.close();
     }
-
-    cout << "##############################################################" << endl;
-    // オートマトンの同期積合成
-    twa_graph_ptr automaton_producted = make_twa_graph(dict);
-    vector<string> name;
-    automaton_producted = synchronous_product(automaton_list, shared, name);
-    cout<< "なまえ" << endl;
-    for (size_t i = 0; i < name.size(); i++) {
-        cout << i << " : " << name[i] << endl;
-    }
-
-    print_hoa(cout, automaton_producted);
-    ofstream producted_ofs("producted.hoa");
-    print_hoa(producted_ofs, automaton_producted);
-    producted_ofs.close();
-
-    automaton_producted->get_dict()->dump(cout);
-
-    return 0;
-
-    vector<bdd> bdd_list = {};    
-    for (auto automaton: automaton_list) {
-        // オートマトンの出力
-        cout << "##############################################################" << endl;
-        print_hoa(cout, automaton);
-        cout << endl;
-
-        for (auto edge: automaton->edges()) {
-            cout << "from: " << edge.src << " to: " << edge.dst << " label: " << edge.cond << endl;
-            bdd_list.push_back(edge.cond);
-        }
-    }
-    bdd tmp = bdd_and(bdd_list[0], bdd_list[4]);
-    bdd zero = bdd_and(tmp, bdd_list[5]);
-    cout << zero << endl;
-
-    // parsed_formula parsed = parse_infix_psl(ltl_formula_str);
-    // formula formula = parsed.f;
-    // cout << "入力 LTL : " << str_psl(formula) << endl;
     cout << "応答イベント : " << endl;
     for (auto res: responseEvents) {
         cout << res << endl;
     }
 
-    // オートマトンの出力
-    //print_dot(cout, automaton_producted);
+    // オートマトンの同期積合成
+    cout << "同期積合成を開始します" << endl;
+    twa_graph_ptr automaton_producted = make_twa_graph(dict);
+    vector<string> name;
+    automaton_producted = synchronous_product(automaton_list, shared, name);
+    cout << "同期積合成が完了しました" << endl;
+    ofstream producted_ofs("producted.hoa");
+    print_hoa(producted_ofs, automaton_producted);
+    producted_ofs.close();
+
+    // デバッグ用
+    cout << "====================DEBUG====================" << endl;
+    cout<< "なまえ" << endl;
+    for (size_t i = 0; i < name.size(); i++) {
+        cout << i << " : " << name[i] << endl;
+    }
+    automaton_producted->get_dict()->dump(cout);
+    print_hoa(cout, automaton_producted);
+
+    return 0;
 }
